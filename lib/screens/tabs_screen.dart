e8cabf75d0c40763a42cab2b48ca3f15d32783f5
evicoach@gmail.com <evicoach@gmail.com>
2020-03-29 07:20:31 +0100

    Added bottom navigation bar and used it as tabs

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,8 +1,38 @@
 /// only manage the tabs and load different screens depending on which tab was selected
 import 'package:flutter/material.dart';
 
-import './categories_screen.dart';
 import './favorites_screen.dart';
+import './categories_screen.dart';
+
+// class TabsScreen extends StatefulWidget {
+//   @override
+//   _TabsScreenState createState() => _TabsScreenState();
+// }
+
+// class _TabsScreenState extends State<TabsScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 2,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Meals'),
+//           bottom: TabBar(
+//             tabs: <Widget>[
+//               Tab(icon: Icon(Icons.category), text: 'Categories'),
+//               Tab(icon: Icon(Icons.star), text: 'Favorites')
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(children: <Widget>[
+//           CategoriesScreen(), FavoritesScreen()
+//         ],),
+//       ),
+//     );
+//   }
+// }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// BOTTOM TAB BAR
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -10,23 +40,43 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, dynamic>> _pages = [
+    {'page': CategoriesScreen(), 'title': 'Categories'},
+    {'page': FavoritesScreen(), 'title': 'Favorites'}
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.category), text: 'Categories'),
-              Tab(icon: Icon(Icons.star), text: 'Favorites')
-            ],
-          ),
-        ),
-        body: TabBarView(children: <Widget>[
-          CategoriesScreen(), FavoritesScreen()
-        ],),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        // type: BottomNavigationBarType.shifting,
+        onTap: (selectedPageIndex) => _selectPage(selectedPageIndex),
+        backgroundColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(
+              // backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.category),
+              title: Text('Category')),
+          BottomNavigationBarItem(
+              // backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.star),
+              title: Text('Favorites'))
+        ],
       ),
     );
   }
